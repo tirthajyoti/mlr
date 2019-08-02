91ff6fb6eecfd314e27c9a54a17d423252768c39
tirthajyoti <tirthajyoti@gmail.com>
2019-08-02 00:02:43 -0700

    Updated setup and added top image

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     install_requires=['numpy','pandas','matplotlib','seaborn','statsmodels'],
     keywords=[
         'Regression',
+        'Linear regression',
         'Data science',
         'Machine learning',
         'Engineering',
@@ -30,8 +31,12 @@ setup(
         ],
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Healthcare Industry',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
